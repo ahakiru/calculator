8cd5723c03b2b87b2f8239e20d17f0a542d4f250
ahakiru <your.maoka@googlemail.com>
2024-08-20 16:38:36 +0200

    Add operator variables and basic operate function.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*math functions*/
 function add (a, b) {
     return a + b;
 };
@@ -12,4 +13,22 @@ function multiply (a, b) {
 
 function divide (a, b) {
     return a / b;
+};
+
+/*math operator variables*/
+let inputA = 0;
+let operator = undefined;
+let inputB = 0;
+
+/* operator function*/
+function operate () {
+    if (operator === '+') {
+        return add(inputA, inputB)
+    } else if (operator === '-') {
+        return subtract (inputA, inputB)
+    } else if (operator === '*') {
+        return multiply (inputA, inputB)
+    } else if (operator === '/') {
+        return divide (inputA, inputB)
+    };
 };
