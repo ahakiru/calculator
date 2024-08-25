c005e0b62ce26ca26dbb4108465464673b1c39c8
ahakiru <your.maoka@googlemail.com>
2024-08-25 19:20:39 +0200

    Enhance functionality, clear button, operate function.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*math functions*/
 function add (a, b) {
-    return a + b;
+    return +a + +b;
 };
 
 function subtract (a, b) {
@@ -12,7 +12,9 @@ function multiply (a, b) {
 };
 
 function divide (a, b) {
-    return a / b;
+    if (inputB === "0") {
+        alert("DANGER!");
+    } else {return a / b}
 };
 
 /*math operator variables*/
@@ -35,7 +37,44 @@ function operate () {
 
 /* get html-elements */
 const display = document.querySelector("#display");
-let buttons = document.querySelectorAll("button");
-buttons.forEach( button => button.addEventListener("click", function () {
-    alert(button.textContent);
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let execute = document.querySelector("#execute");
+let remove = document.querySelector("#remove");
+let clear = document.querySelector("#clear");
+
+numbers.forEach(button => button.addEventListener("click", function () {
+    if (display.textContent === "0") {
+        display.textContent = button.textContent;
+    } else {display.textContent += button.textContent;}
+    if (operator === undefined) {
+        inputA = display.textContent;
+    } else {
+        inputB = display.textContent;
+    };
+    }
+));
+
+operators.forEach(button => button.addEventListener("click", function () {
+    if (operator === undefined) {
+        operator = button.textContent;
+        display.textContent = "0";
+    } else {
+        display.textContent = operate (inputA, operator, inputB);
+        inputA = display.textContent;
+        operator = button.textContent;
+        display.textContent = "0";
+    }
+
 }));
+
+execute.addEventListener("click", function () {
+    display.textContent = operate (inputA, operator, inputB);
+})
+
+clear.addEventListener("click", function() {
+    inputA = 0;
+    inputB = 0;
+    operator = undefined;
+    display.textContent = "0";
+})
